ed76cf3c2fd85313a75b7725b9310386263d440b
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2023-11-16 23:01:17 +0600

    added jwt

--- Bristo_server/index.js
+++ Bristo_server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors =require('cors')
 require('dotenv').config()
+const jwt =require('jsonwebtoken')
 const port = process.env.PORT || 3000
  app.use(express.json())
  app.use(cors())
@@ -33,8 +34,20 @@ async function run() {
     const cartCollection = client.db("bristoDB").collection("cart");
     const userCollection = client.db("bristoDB").collection("user");
 
+
+    // console.log(process.env.ACCESS_TOKEN_SECRET)
+    // jwt related api 
+
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body
+    const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'10h'});
+    res.send({token})
+
+    })
+
     // user realeted api 
     app.post('/users',async(req,res)=>{
+
       const user = req.body;
       // dublicate user ke bad dibo 
       const query={email:user.email}
@@ -46,9 +59,46 @@ async function run() {
       const result = await userCollection.insertOne(user)
       res.send(result);
     })
-    app.get('/users',async(req,res)=>{
+// middlewares 
+const verifyToken= (req,res,next)=>{
+  console.log('inside token authorization',req.headers.authorization)
+  if(!req.headers.authorization){
+    return res.status(401).send({message:'unathorize error'})
+  }
+  const token = req.headers.authorization.split(' ')[1]
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+  if(err){
+    return res.status(403).send({message:'unathorize error'})
+  }
+  req.decoded = decoded;
+  next()
+  })
+
+  // next()
+}
+
+    app.get('/users',verifyToken,async(req,res)=>{
+      
      const result = await userCollection.find().toArray()
      res.send(result)
+    })
+
+
+    // find admin or verify admin 
+    app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+  const email =req.params.email;
+  if(email !==req.decoded.email){
+    return res.status(403).send({message:'unathorize error'})
+  }
+  const query ={email:email}
+  const user = await userCollection.findOne(query)
+  let admin = false;
+  if(user){
+    admin = user?.role ==='admin';
+
+  }
+  res.send({admin})
+
     })
     // delete user for all user .....route 
     app.delete('/users/:id',async(req,res)=>{
